a64a4b2fa5f6cac302daae478da95d5cdcb46253
Alexander Neumann <aleneum@gmail.com>
2024-02-28 18:45:49 +0100

    add default to LoraModules. values can specificy overide default

--- cto_notebooks/utils/lora.py
+++ cto_notebooks/utils/lora.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass, field
-from typing import Dict, List, Protocol
+from typing import Dict, List, Optional, Protocol
 
 # from peft.utils.other import (
 #     TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING as model_to_lora_modules,
@@ -16,20 +16,22 @@ class LLMModelProto(Protocol):
 
 @dataclass
 class LoraModules:
-    q: bool = True
-    v: bool = True
-    k: bool = True
-    o: bool = True
-    gate: bool = True
-    down: bool = True
-    up: bool = True
+    q: Optional[bool] = None
+    v: Optional[bool] = None
+    k: Optional[bool] = None
+    o: Optional[bool] = None
+    gate: Optional[bool] = None
+    down: Optional[bool] = None
+    up: Optional[bool] = None
+    default: bool = False
 
     def target_modules(self, model: LLMModelProto) -> List[str]:
         avail = find_target_modules(model)
         return [
             f"{name}_proj"
             for name, enabled in asdict(self).items()
-            if enabled and f"{name}_proj" in avail
+            if (enabled is True or (enabled is None and self.default is True))
+            and f"{name}_proj" in avail
         ]
 
 
@@ -37,32 +39,34 @@ class LoraModules:
 class TrainingFlags:
     use_cpu: bool = False
     use_bf16: bool = False
-    use_ipex: bool = (
-        False  # True if is_torch_xpu_available() and not USE_CPU else False
-    )
+    use_ipex: bool = False
+    # True if is_torch_xpu_available() and not USE_CPU else False
 
 
 @dataclass
 class LoraTrainingConfig:
     model: Dict[str, str]
+    output_dir: str
 
-    cutoff_len = 256
-    overlap_len = 128
-    train_only_after = ""
-    lora_rank = 32
-    lora_alpha = 64
-    lora_dropout = 0.05
-    save_steps = 0  # Save every n steps
-    warmup_steps = 100
-    epochs = 3
+    cutoff_len: int = 256
+    overlap_len: int = 128
+    train_only_after: str = ""
+    lora_rank: int = 32
+    lora_alpha: int = 64
+    lora_dropout: float = 0.05
+    save_steps: int = 500  # Save every n steps
+    warmup_steps: int = 100
+    epochs: int = 3
+    bias: str = "none"
+    task_type: str = "CAUSAL_LM"
 
     # learning_rate
     # 3e-4 is a good starting base point.
     # 1e-2 is extremely high, 1e-6 is extremely low.
-    learning_rate = 3e-4
-    batch_size = 128
-    micro_batch_size = 4
-    stop_at_loss = 0  # (reasonable numbers are 1.5-1.8)
+    learning_rate: float = 3e-4
+    batch_size: int = 128
+    micro_batch_size: int = 4
+    stop_at_loss: float = 0  # (reasonable numbers are 1.5-1.8)
 
     # optimizer = [
     #     'adamw_hf', 'adamw_torch', 'adamw_torch_fused', 'adamw_torch_xla',
@@ -70,7 +74,7 @@ class LoraTrainingConfig:
     #     'sgd', 'adagrad']
     # Different optimizer implementation options, for advanced users.
     # Effects of different options are not well documented yet.',
-    optimizer = "adamw_torch"
+    optimizer: str = "adamw_torch"
 
     # lr_scheduler = [
     #    'linear', 'constant', 'constant_with_warmup', 'cosine',
@@ -78,7 +82,7 @@ class LoraTrainingConfig:
     # Learning rate scheduler - defines how the learning rate changes over time.
     # "Constant" means never change, "linear" means to go in a straight line from the
     # learning rate down to 0, cosine follows a curve, etc.'
-    lr_scheduler = "linear"
+    lr_scheduler: str = "linear"
 
     modules: LoraModules = field(default_factory=LoraModules)
     flags: TrainingFlags = field(default_factory=TrainingFlags)
