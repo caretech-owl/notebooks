12d771ec23f60dd4914eb6a5c3317d6410a5816c
Alexander Neumann <aleneum@gmail.com>
2024-03-27 00:11:05 +0100

    add similarity_search_by_vector to vector protocol; and use array content for average_vector

--- cto_notebooks/utils/rag.py
+++ cto_notebooks/utils/rag.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable, List, Protocol, Tuple
+from typing import Dict, Iterable, List, Protocol, Tuple, Union
 
 import numpy as np
 from langchain.docstore.document import Document
@@ -21,22 +21,30 @@ class VectorStore(Protocol):
     def search(self, query: str, search_type: str, k: int) -> List[Document]:
         pass
 
+    def similarity_search_by_vector(
+        self, vector: List[float], k: int
+    ) -> List[Document]:
+        pass
+
 
 def average_vector(
     field: str,
     embeddings: HuggingFaceEmbeddings,
-    data_dict: Iterable[Tuple[Dict[str, str], VectorStore]],
+    data_dict: Iterable[Tuple[Dict[str, Union[str, List[str]]], VectorStore]],
     initial_query: str = "",
     k: int = 10,
 ) -> List[float]:
     vecs = []
     for dict, store in data_dict:
+        values = dict[field]
+        if isinstance(values, str):
+            values = [values]
         for vec in store.search(
-            f"{initial_query} {dict[field]}",
+            f"{initial_query} {', '.join(values)}",
             search_type="similarity",
             k=k,
         ):
-            if dict[field] not in vec.page_content:
+            if not any(val in vec.page_content for val in values):
                 continue
             vecs.append(embeddings.embed_query(vec.page_content))
             break
